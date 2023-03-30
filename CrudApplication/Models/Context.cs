7aee16c2930b4eb45f059fa3a768c3fd70a19409
Márcio Santos <saturno17.mv@gmail.com>
2023-03-30 20:11:42 -0300

    Adicionar arquivos de projeto.

---
+++ CrudApplication/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudApplication.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options):base(options) 
+        { 
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ CrudApplication/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudApplication.Models
+{
+    [Table("Produto")]
+    public class Product
+    {
+        [Column("Id")]
+        [Display(Name = "ProductID")]
+        public int Id { get; set; }
+
+        [Column("Name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
